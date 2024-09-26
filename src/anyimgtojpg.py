32c046df633ddeb9a2fc516b8818138a2f82f718
Diego Miguel M. Villamil <dmvillamil@up.edu.ph>
2024-09-27 01:10:50 +0800

    applied black and isort formatting

--- src/anyimgtojpg.py
+++ src/anyimgtojpg.py
@@ -1,10 +1,11 @@
-from PIL import Image
-import os
 import logging
-
+import os
 import tkinter as tk
-from tkinter import filedialog
 from functools import partial
+from tkinter import filedialog
+
+from PIL import Image
+
 
 # uses src_path as the directory source for images
 # then saves them to out_path as .jpg files
@@ -16,7 +17,7 @@ def allImgToJpg(src_label: tk.Label, out_label: tk.Label, err_label: tk.Label) -
     unsupported_files = []
 
     for file in f_names:
-        if(file == '.gitkeep'):
+        if file == ".gitkeep":
             continue
 
         new_f_name = file.split(".")[0] + ".jpg"
@@ -28,39 +29,55 @@ def allImgToJpg(src_label: tk.Label, out_label: tk.Label, err_label: tk.Label) -
             logging.error("The file: " + file + " was not an image")
             continue
 
-        img = img.convert('RGB')
+        img = img.convert("RGB")
         try:
-            img.save(out_path + new_f_name , "JPEG")
+            img.save(out_path + new_f_name, "JPEG")
         except:
             logging.error("Error: The file:" + new_f_name + "could not be saved")
-            err_label.config(text="Error: The file:" + new_f_name + "could not be saved")
+            err_label.config(
+                text="Error: The file:" + new_f_name + "could not be saved"
+            )
 
-    print("Convertion complete.")    
+    print("Convertion complete.")
+
 
 # changes the *_selected_label text attribute to the selected directory
 def get_dir_path(path_label: tk.Label) -> None:
     path_label.config(text=filedialog.askdirectory())
 
+
 # Start of tkinter gui
 root = tk.Tk()
 root.title("anyimgtojpg")
 
-src_dialog_label = tk.Label(root, text="Select the folder with the images to be converted:")
+src_dialog_label = tk.Label(
+    root, text="Select the folder with the images to be converted:"
+)
 src_selected_label = tk.Label(root)
-src_dialog_btn = tk.Button(root, text="Input Folder", command=partial(get_dir_path, src_selected_label))
+src_dialog_btn = tk.Button(
+    root, text="Input Folder", command=partial(get_dir_path, src_selected_label)
+)
 src_dialog_label.pack()
 src_dialog_btn.pack()
 src_selected_label.pack()
 
-out_dialog_label = tk.Label(root, text="Select the folder where the images will be saved")
+out_dialog_label = tk.Label(
+    root, text="Select the folder where the images will be saved"
+)
 out_selected_label = tk.Label(root)
-out_dialog_btn = tk.Button(root, text="Output Folder", command=partial(get_dir_path, out_selected_label))
+out_dialog_btn = tk.Button(
+    root, text="Output Folder", command=partial(get_dir_path, out_selected_label)
+)
 out_dialog_label.pack()
 out_dialog_btn.pack()
 out_selected_label.pack()
 
 err_label = tk.Label(root, text="")
-conv_files_btn = tk.Button(root, text="Convert Images", command=partial(allImgToJpg, src_selected_label, out_selected_label, err_label))
+conv_files_btn = tk.Button(
+    root,
+    text="Convert Images",
+    command=partial(allImgToJpg, src_selected_label, out_selected_label, err_label),
+)
 conv_files_btn.pack()
 err_label.pack()
 
