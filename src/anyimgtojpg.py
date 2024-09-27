a41c723ca4ecab189db5da28a6a57fb737bc5b5f
Diego Miguel M. Villamil <dmvillamil@up.edu.ph>
2024-09-27 21:50:41 +0800

    error.log logs all files not converted and directories found and files are copied to the err dir

--- src/anyimgtojpg.py
+++ src/anyimgtojpg.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import shutil
 import tkinter as tk
 from functools import partial
 from tkinter import filedialog
@@ -9,18 +10,20 @@ from PIL import Image
 
 # uses src_path as the directory source for images
 # then saves them to out_path as .jpg files
-def allImgToJpg(src_label: tk.Label, out_label: tk.Label, err_label: tk.Label) -> None:
+def allImgToJpg(
+    src_label: tk.Label,
+    out_label: tk.Label,
+    status_label: tk.Label,
+    err_label: tk.Label,
+) -> None:
     print("Converting images to jpg...")
     src_path = src_label["text"] + "\\"
     out_path = out_label["text"] + "\\"
-    blank_dirs = []
+    err_path = "err\\"
 
-    if src_path == "\\":
-        blank_dirs.append(src_path)
-    if out_path == "\\":
-        blank_dirs.append(out_path)
-    if len(blank_dirs) > 0:
-        logging.error("Error: the following input folders are blank: {}", blank_dirs)
+    if src_path == "\\" or out_path == "\\":
+        err_label.config(text="The input or ouput directory is blank")
+        return
 
     f_names = os.listdir(src_path)
     unsupported_files = []
@@ -30,12 +33,18 @@ def allImgToJpg(src_label: tk.Label, out_label: tk.Label, err_label: tk.Label) -
             continue
 
         new_f_name = file.split(".")[0] + ".jpg"
+        file_path = src_path + file
         try:
-            img = Image.open(src_path + file)
+            img = Image.open(file_path)
         except:
+            if os.path.isdir(file_path):
+                logging.error(file + " was a folder")
+                continue
+
             print("Error: The file: " + file + " was not an image")
             unsupported_files.append(file)
             logging.error("The file: " + file + " was not an image")
+            shutil.copy(file_path, err_path)
             continue
 
         img = img.convert("RGB")
@@ -47,6 +56,10 @@ def allImgToJpg(src_label: tk.Label, out_label: tk.Label, err_label: tk.Label) -
                 text="Error: The file:" + new_f_name + "could not be saved"
             )
 
+    status_label["text"] = "Convertion task complete"
+    err_count = len(unsupported_files)
+    if err_count > 0:
+        err_label["text"] = str(err_count) + " files were not converted"
     print("Convertion complete.")
 
 
@@ -54,6 +67,13 @@ def allImgToJpg(src_label: tk.Label, out_label: tk.Label, err_label: tk.Label) -
 def get_dir_path(path_label: tk.Label) -> None:
     path_label.config(text=filedialog.askdirectory())
 
+
+# Python logging configuration
+logging.basicConfig(
+    filename="err/error.log",
+    filemode="w",
+    format="%(name)s → %(levelname)s: %(message)s",
+)
 
 # Start of tkinter gui
 
@@ -86,13 +106,19 @@ out_dialog_label.pack()
 out_dialog_btn.pack()
 out_selected_label.pack()
 
+status_label = tk.Label(root, text="")
+status_label.config(fg="#0A7A16")
 err_label = tk.Label(root, text="")
+status_label.config(fg="#FF0000")
 conv_files_btn = tk.Button(
     root,
     text="Convert Images",
-    command=partial(allImgToJpg, src_selected_label, out_selected_label, err_label),
+    command=partial(
+        allImgToJpg, src_selected_label, out_selected_label, status_label, err_label
+    ),
 )
 conv_files_btn.pack()
+status_label.pack()
 err_label.pack()
 
 root.mainloop()
