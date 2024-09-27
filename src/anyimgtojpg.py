5e6ae3a7f83d17003da0d089d6da694efdcb9e2c
Diego Miguel M. Villamil <dmvillamil@up.edu.ph>
2024-09-27 17:36:44 +0800

    added pysinstaller compiled exe

--- src/anyimgtojpg.py
+++ src/anyimgtojpg.py
@@ -13,6 +13,15 @@ def allImgToJpg(src_label: tk.Label, out_label: tk.Label, err_label: tk.Label) -
     print("Converting images to jpg...")
     src_path = src_label["text"] + "\\"
     out_path = out_label["text"] + "\\"
+    blank_dirs = []
+
+    if src_path == "\\":
+        blank_dirs.append(src_path)
+    if out_path == "\\":
+        blank_dirs.append(out_path)
+    if len(blank_dirs) > 0:
+        logging.error("Error: the following input folders are blank: {}", blank_dirs)
+
     f_names = os.listdir(src_path)
     unsupported_files = []
 
@@ -47,13 +56,18 @@ def get_dir_path(path_label: tk.Label) -> None:
 
 
 # Start of tkinter gui
+
+# Window Configuration
 root = tk.Tk()
 root.title("anyimgtojpg")
+root.geometry("400x400")
+root.resizable(width=False, height=False)
 
+# User gui
 src_dialog_label = tk.Label(
     root, text="Select the folder with the images to be converted:"
 )
-src_selected_label = tk.Label(root)
+src_selected_label = tk.Label(root, text="")
 src_dialog_btn = tk.Button(
     root, text="Input Folder", command=partial(get_dir_path, src_selected_label)
 )
@@ -64,7 +78,7 @@ src_selected_label.pack()
 out_dialog_label = tk.Label(
     root, text="Select the folder where the images will be saved"
 )
-out_selected_label = tk.Label(root)
+out_selected_label = tk.Label(root, text="")
 out_dialog_btn = tk.Button(
     root, text="Output Folder", command=partial(get_dir_path, out_selected_label)
 )
